858538934bc73035d246b0695253457ede2133ae
Eddy van der Veen <eddy.van.der.veen@ordina.nl>
2019-05-03 08:16:53 +0200

    Lesson 1: A. Created objects within own application code

--- src/main/java/nl/ordina/petstore/App.java
+++ src/main/java/nl/ordina/petstore/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+//        System.out.println( "Hello World!" );
+        // Lesson A
+        PetService petService = new PetService();
+        PetRepo petRepo = new PetRepo();
+
+        System.out.println("petService " + petService.toString());
+        System.out.println("petRepo " + petRepo.toString());
     }
 }
---
+++ src/main/java/nl/ordina/petstore/PetRepo.java
@@ -0,0 +1,4 @@
+package nl.ordina.petstore;
+
+public class PetRepo {
+}
